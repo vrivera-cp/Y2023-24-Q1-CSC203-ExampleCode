eab672d4aaad2845f705a01bf5b76648f7410074
Vanessa Rivera <10712121+rivessa@users.noreply.github.com>
2023-10-16 10:53:00 -0700

    added toString() to Animal

--- Module04/Animal.java
+++ Module04/Animal.java
@@ -18,18 +18,23 @@ public class Animal {
     }
 
     @Override
+    public String toString() {
+        return "Animal{" +
+                "species='" + species + '\'' +
+                ", population=" + population +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Animal animal = (Animal) o;
-        return population == animal.population && Objects.equals(species, animal.species);
+        return population == animal.population && species.equals(animal.species);
     }
 
     @Override
     public int hashCode() {
-        int hash = 1;
-        hash = hash * 31 + (species != null ? species.hashCode() : 0);
-        hash = hash * 31 + population;
-        return hash; // or return Objects.hash(species, population);
+        return Objects.hash(species, population);
     }
 }

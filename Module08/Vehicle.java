ce0dc5f05820fed195730c779083eb2ccbd189bf
Vanessa Rivera <10712121+rivessa@users.noreply.github.com>
2023-11-28 21:55:46 -0800

    Add Project 2 refactoring examples

---
+++ Module08/Vehicle.java
@@ -0,0 +1,58 @@
+public class Vehicle {
+    public VehicleType type;
+    public double batteryCapacity;
+    public double fuel;
+    public Point position;
+    public double speed;
+    public int tires;
+
+    void applyBrakes() { }
+
+    void changeTires() {}
+
+    void dock() {
+        switch (type) {
+            case ROWBOAT:
+                dockToPort();
+                break;
+            case SPACE_SHUTTLE:
+                dockToSpaceStation();
+                break;
+            default:
+                throw new UnsupportedOperationException(String.format(
+                        "Action not supported for %s",
+                        type
+                ));
+        }
+    }
+    void dockToPort() {}
+    void dockToSpaceStation() {}
+
+    void move() {
+        switch (type) {
+            case BICYCLE:
+            case ELECTRIC_CAR:
+                moveWheels();
+                break;
+            case ROWBOAT:
+                moveWater();
+                break;
+            case SPACE_SHUTTLE:
+                moveSpace();
+                break;
+            case TRAIN:
+                moveTrain();
+            default:
+                throw new UnsupportedOperationException(String.format(
+                   "Action not supported for %s",
+                   type
+                ));
+        }
+    }
+    void moveTrain() {}
+    void moveWheels() {}
+    void moveWater() {}
+    void moveSpace() {}
+
+    void refuel() {}
+}
---
+++ Module08/VehicleType.java
@@ -0,0 +1,7 @@
+public enum VehicleType {
+  BICYCLE,
+  ELECTRIC_CAR,
+  ROWBOAT,
+  SPACE_SHUTTLE,
+  TRAIN
+}
